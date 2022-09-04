7e6b082acf71f69287e42c929e2e86a12dd4f89e
sbswag9@gmail.com <sbswag9@gmail.com>
2022-09-04 15:57:58 +0600

    added modal with newsDetails api

--- index.js
+++ index.js
@@ -53,26 +53,36 @@ const displayNews = (newsData) => {
       class="w-full h-[340px] rounded-lg bg-white p-5 flex flex-row items-center"
       >
         <img
-          src="${news.thumbnail_url}"
+          src="${
+            news.thumbnail_url === null ? "unavailable" : news.thumbnail_url
+          }"
           alt=""
           class="w-[244px] h-[300px] mr-10 rounded-md"
         />
         <div class="flex flex-col gap-10 w-full">
           <h2 class="text-2xl font-bold">
-            ${news.title}
+            ${news.title === null ? "unavailable" : news.title}
           </h2>
           <p class="text-lg font-normal text-gray-400 line-clamp-4">
-            ${news.details}
+            ${news.details === null ? "unavailable" : news.details}
           </p>
           <div class="flex justify-between items-center">
             <div class="flex flex-row justify-center items-center">
-              <img src="${news.author.img}" alt="" class="w-[40px] h-[40px] mr-2" />
+              <img src="${
+                news.author.img === null ? "unavailable" : news.author.img
+              }" alt="" class="w-[40px] h-[40px] mr-2" />
               <div class="flex flex-col">
-                <span>${news.author.name}</span>
+                <span>${
+                  news.author?.name === null || news.author.name.length === 0
+                    ? "unavailable"
+                    : news.author.name
+                }</span>
                 <span>${newDate}</span>
               </div>
             </div>
-            <i class="fa-regular fa-eye"> ${news.total_view}</i>
+            <i class="fa-regular fa-eye"> ${
+              news.total_view === null ? "unavailable" : news.total_view
+            }</i>
             <div>
               <i class="fa-solid fa-star"></i>
               <i class="fa-solid fa-star-half-stroke"></i>
@@ -80,7 +90,8 @@ const displayNews = (newsData) => {
               <i class="fa-regular fa-star"></i>
               <i class="fa-regular fa-star"></i>
             </div>
-            <i class="fa-solid fa-arrow-right text-purple-700 cursor-pointer"></i>
+            <button onClick="newsDetails('${news._id}')" data-bs-toggle="modal"
+            data-bs-target="#exampleModalCenteredScrollable" type="button"><i class="fa-solid fa-arrow-right text-purple-700 cursor-pointer"></i></button>
           </div>
         </div>
       </div>
@@ -88,5 +99,44 @@ const displayNews = (newsData) => {
 
     newsContainer.appendChild(newsCard);
     loader.classList.add("hidden");
+  }
+};
+
+const newsDetails = async (newsId) => {
+  let modalContainer = document.getElementById("modal-container");
+  try {
+    let res = await fetch(`${baseUrl}/${newsId}`);
+    let data = await res.json();
+    let newsData = data.data[0];
+    modalContainer.innerHTML = `
+    <div class="flex flex-col gap-10 w-full">
+      <h2 class="text-2xl font-bold">
+        ${newsData.title === null ? "unavailable" : newsData.title}
+      </h2>
+      <p class="text-lg font-normal text-gray-400 ">
+        ${newsData.details === null ? "unavailable" : newsData.details}
+      </p>
+      <div class="flex justify-between items-center">
+        <div class="flex flex-row justify-center items-center">
+          <img src="${
+            newsData.author.img === null ? "unavailable" : newsData.author.img
+          }" alt="" class="w-[40px] h-[40px] mr-2" />
+          <div class="flex flex-col">
+            <span>${
+              newsData.author?.name === null ||
+              newsData.author.name.length === 0
+                ? "unavailable"
+                : newsData.author.name
+            }</span>
+            <span>${newsData.author.published_date}</span>
+          </div>
+        </div>
+        <i class="fa-regular fa-eye"> ${
+          newsData.total_view === null ? "unavailable" : newsData.total_view
+        }</i>
+    </div>
+  `;
+  } catch (error) {
+    console.log(error);
   }
 };
