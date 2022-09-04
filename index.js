41b6807c5922dd7c9762abe6119c58fa1f12c5da
sbswag9@gmail.com <sbswag9@gmail.com>
2022-09-04 10:56:20 +0600

    added main news section with js functionality

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ const displayCategories = (categories) => {
 };
 
 const getCategoryNews = (categoryId, categoryName) => {
+  const loader = document.getElementById("loader");
+  loader.classList.remove("hidden");
   const categoryInfo = document.getElementById("categoryInfo");
   fetch(`${baseUrl}/category/${categoryId}`)
     .then((res) => res.json())
@@ -36,4 +38,52 @@ const getCategoryNews = (categoryId, categoryName) => {
     });
 };
 
-const displayNews = (newsData) => {};
+const displayNews = (newsData) => {
+  const newsContainer = document.getElementById("news");
+  newsContainer.textContent = "";
+  const loader = document.getElementById("loader");
+
+  for (news of newsData) {
+    const newsCard = document.createElement("div");
+    newsCard.innerHTML = `
+      <div
+      class="w-full h-[340px] rounded-lg bg-white p-5 flex flex-row justify-center items-center"
+      >
+        <img
+          src="${news.thumbnail_url}"
+          alt=""
+          class="w-[244px] h-[300px] mr-10 rounded-md"
+        />
+        <div class="flex flex-col gap-10">
+          <h2 class="text-2xl font-bold">
+            ${news.title}
+          </h2>
+          <p class="text-lg font-nomal text-gray-400">
+            ${news.details}
+          </p>
+          <div class="flex justify-between items-center">
+            <div class="flex flex-row justify-center items-center">
+              <img src="${news.author.img}" alt="" class="w-[40px] h-[40px] mr-2" />
+              <div class="flex flex-col">
+                <span>${news.author.name}</span>
+                <span>${news.author.published_date}</span>
+              </div>
+            </div>
+            <i class="fa-regular fa-eye">${news.total_view}</i>
+            <div>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star-half-stroke"></i>
+              <i class="fa-regular fa-star"></i>
+              <i class="fa-regular fa-star"></i>
+              <i class="fa-regular fa-star"></i>
+            </div>
+            <i class="fa-solid fa-arrow-right text-purple-700 cursor-pointer"></i>
+          </div>
+        </div>
+      </div>
+    `;
+
+    newsContainer.appendChild(newsCard);
+    loader.classList.add("hidden");
+  }
+};
